cdac9b7f8c424c344415cf95dd40249961e8e78e
lucwise-13257 <luca_lise@icloud.com>
2024-08-30 12:49:13 -0700

    Remove import testing

--- handlers/slack/index.js
+++ handlers/slack/index.js
@@ -1,10 +1,3 @@
-const testFolder = "/opt/";
-const fs = require("fs");
-
-fs.readdirSync(testFolder).forEach((file) => {
-  console.log(file);
-});
-
 const { verifyHeaders } = require("/opt/slackutils");
 const handler = async (event, context) => {
   verifyHeaders(event.headers, event.body);
