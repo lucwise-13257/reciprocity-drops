a84daa65d177f1d791b753ff6a0255f73dfd7cc3
lucwise-13257 <luca_lise@icloud.com>
2024-08-30 12:27:49 -0700

    Debugging import issue

--- handlers/slack/index.mjs
+++ handlers/slack/index.mjs
@@ -1,4 +1,11 @@
-import { verifyHeaders } from "/opt/slackUtils.mjs";
+const testFolder = "/opt/";
+import fs from "fs";
+
+fs.readdirSync(testFolder).forEach((file) => {
+  console.log(file);
+});
+
+import { verifyHeaders } from "/opt/slackutils";
 export const handler = async (event, context) => {
   verifyHeaders(event.headers, event.body);
   return {
