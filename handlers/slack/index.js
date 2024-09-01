9e3dc9e190a29d6b980c453b6dc9557b07effbd7
lucwise-13257 <luca_lise@icloud.com>
2024-09-01 00:19:55 -0700

    SQS Progress

--- handlers/slack/index.js
+++ handlers/slack/index.js
@@ -3,24 +3,51 @@ const { verifyHeaders } = require("/opt/slackutils");
 const handler = async (event, context) => {
   const verificationResult = verifyHeaders(event.headers, event.body);
   if (verificationResult !== true) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        response_type: "in_channel",
-        blocks: [
-          {
-            type: "section",
-            text: {
-              type: "plain_text",
-              text: verificationResult,
-              emoji: true,
-            },
+    return sendResponse(200, {
+      response_type: "ephemeral",
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "plain_text",
+            text: verificationResult,
+            emoji: true,
           },
-        ],
-      }),
-    };
+        },
+      ],
+    });
   }
+  // Send message to SQS Queue
+
+  // Inform user the request was recieved.
+  return sendResponse(200, {
+    response_type: "in_channel",
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "plain_text",
+          text: "Processing request.",
+          emoji: true,
+        },
+      },
+    ],
+  });
 };
+
+/**
+ * Sends a response with the specified status code and body.
+ *
+ * @param {number} code - The status code of the response.
+ * @param {any} body - The body of the response.
+ * @returns {object} - The response object.
+ */
+function sendResponse(code, body) {
+  return {
+    statusCode: code,
+    body: JSON.stringify(body),
+  };
+}
 
 module.exports = {
   handler,
